619ed67a8235b00c3599d7855e2fea07484dbbb7
Phan2m Ghost <phantomghost.v1@gmail.com>
2023-10-16 15:09:28 +0800

    Edit ascend sort.py

--- sort.py
+++ sort.py
@@ -1,10 +1,22 @@
 import re
 
-uInput = input('Enter Nynbers: ')
+uInput = input('Enter Numbers: ')
 
 #input to list
 nList = re.split(',|-|_', uInput)
 
 #str to int
-nInt = [int(n) for n in nList]
-print(f"Your Numbers: {nInt}")
+nInt_1 = [int(n) for n in nList]
+print(f"Your Numbers: {nInt_1}")
+nInt_2 = nInt_1[:]
+
+#ascending
+nOutput = []
+while len(nInt_1) != 0:
+    first_1 = nInt_1[0]
+    for i in nInt_1:
+        if (i < first_1):
+            first_1 = i
+    nOutput.append(first_1)
+    nInt_1.remove(first_1)
+print(f"Ascending: {nOutput}")
