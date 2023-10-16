8658f33de4836c0e6113474d776eae6e1816a585
Phan2m Ghost <phantomghost.v1@gmail.com>
2023-10-16 15:15:09 +0800

    Edit descending sort.py

--- sort.py
+++ sort.py
@@ -11,12 +11,23 @@ print(f"Your Numbers: {nInt_1}")
 nInt_2 = nInt_1[:]
 
 #ascending
-nOutput = []
+nOutput_1 = []
 while len(nInt_1) != 0:
     first_1 = nInt_1[0]
     for i in nInt_1:
         if (i < first_1):
             first_1 = i
-    nOutput.append(first_1)
+    nOutput_1.append(first_1)
     nInt_1.remove(first_1)
-print(f"Ascending: {nOutput}")
+print(f"Ascending: {nOutput_1}")
+
+#descending
+nOutput_2 = []
+while len(nInt_2) != 0:
+    first_2 = nInt_2[0]
+    for i in nInt_2:
+        if (i > first_2):
+            first_2 = i
+    nOutput_2.append(first_2)
+    nInt_2.remove(first_2)
+print(f"Descending: {nOutput_2}")
