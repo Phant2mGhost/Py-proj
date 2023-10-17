b70b959af55ac2059fd692d692c183151215dbe5
Phan2m Ghost <phantomghost.v1@gmail.com>
2023-10-17 13:16:05 +0800

    Edit rewrite entrep.py

--- entrep.py
+++ entrep.py
@@ -1,5 +1,8 @@
-file = open("entrep.txt", "r")
+file = open("entrep.txt", "r+")
 lines = file.read()
 new_lines = lines.replace("**", "")
+file.seek(0) #moves file pointer to the beginning 
+file.write(new_lines)
+file.truncate() #removes everything after the new lines
 print(new_lines)
 file.close()
