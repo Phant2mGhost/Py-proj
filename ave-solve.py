2bf84cf3496e9d931dab76efd4259be70d896505
Phan2m Ghost <phantomghost.v1@gmail.com>
2023-10-25 14:35:26 +0800

    Ready for 1 person ave-solve.py
    Switch variables of fastest and slowest
    Add if statement for DNF solve/s
    Improve readabilut with =
    Fix bug

--- ave-solve.py
+++ ave-solve.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-#input to list
-user_input = input("Enter time separated with spaces:\n")
+#input to str list
+user_input = input("(Enter time separated with spaces)\n================================\n")
 strList = user_input.split()
-intList = list(map(float, strList))
+
+#str to int
+dnf = "DNF"
+nStrList = []
+for s in strList:
+    if s == dnf:
+        s = "1000000"
+    nStrList.append(s)
+intList = list(map(float, nStrList))
 
 #fastest and slowest
-fastest = max(intList)
-slowest = min(intList)
+slowest = max(intList)
+fastest = min(intList)
 
 #ave
 intList.remove(fastest)
@@ -15,7 +23,14 @@ intList.remove(slowest)
 ave = np.mean(intList)
 
 #display time
-solves = str(len(strList)+1)
+solves = str(len(strList))
+print("================================")
 print(f"Fastest: {fastest}")
-print(f"Slowest: {slowest}")
+print("================================")
+if slowest == 1000000:
+    print(f"Slowest: {dnf}")
+else:
+    print(f"Slowest: {slowest}")
+print("================================")
 print(f"Ao{solves}: {ave}")
+print("================================")
